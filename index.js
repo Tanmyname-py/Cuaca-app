1b56f9c15d901eaf6359c2d97305c9772659dfd6
YoDev&! <tanxzxzxzxz@gmail.com>
2026-09-25 07:22:48 +0800

    Modifikasi ulang style dan interaktif card-cuaca dan menambahkan assets gambar baru

--- index.js
+++ index.js
@@ -51,42 +51,79 @@ inputKecamatan.addEventListener('change', async (e) => {
 
 
 inputDesa.addEventListener('change', async (e) => {
+    function formatWaktu(time) {
+        return time < 10 ? time.toString().padStart(2, '0') : time;
+    }
     const code = e.target.value;
-    const cuaca = await fetch(`http://localhost:3000/cuaca?code=${code}`);
-    if (cuaca.ok) {
-        let result = await cuaca.json();
-        const detail = [
-            { label: 'Kelembapan', nilai: `${result.data[0].cuaca[0][0].hu}%` },
-            { label: 'Angin', nilai: `${result.data[0].cuaca[0][0].ws} km/jam` }
-        ]
-        const lokasi = document.querySelector('.loc');
-        const suhu = document.querySelector('.suhu');
-        const desc_cuaca = document.querySelector('.desc-cuaca')
-        lokasi.textContent = `${result.lokasi.kecamatan}, ${result.lokasi.desa}`
-        suhu.innerHTML = `${result.data[0].cuaca[0][0].t} &deg;`
-        desc_cuaca.textContent = result.data[0].cuaca[0][0].weather_desc
-        const html = detail.map(d => `
-            <li>
-            <span class="label">${d.label}</span>
-            <span class="nilai">${d.nilai}</span>
-            </li>
-        `).join("");
-        document.querySelector('.detail').innerHTML = html;
-        switch (result.data[0].cuaca[0][0].weather_desc) {
-            case 'Berawan': {
-                document.querySelector('.header-card').style.backgroundImage = `url(./assets/cloudy.jpg)`;
-                break
+    let perkiraanSuhu = '';
+    const hari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
+    const reqcuaca = await fetch(`http://localhost:3000/cuaca?code=${code}`);
+    if (reqcuaca.ok) {
+        document.querySelector('.card-cuaca').classList.add('display');
+        let date;
+        let idxCuaca = 0;
+        let result = await reqcuaca.json();
+        const cuaca = result.data[0].cuaca[0];
+        for (let dataCuaca of cuaca) {
+            idxCuaca++;
+            let local_datetime = new Date(dataCuaca.local_datetime);
+            let now = new Date();
+            if (local_datetime.getDate() === now.getDate() && local_datetime.getHours() > now.getHours()) {
+                date = new Date(dataCuaca.local_datetime)
+                break;
+            } else {
+                date = new Date();
             }
-            case 'Cerah Berawan': {
-                document.querySelector('.header-card').style.backgroundImage = `url(./assets/sunny.jpg)`;
+        }
+
+        const kecamatan = result.lokasi.kecamatan;
+        const desa = result.lokasi.desa;
+        let suhu = cuaca[idxCuaca].t;
+        let weather_desc = cuaca[idxCuaca].weather_desc;
+        const statistik = [
+            { label: 'Kelembapan', value: `${cuaca[idxCuaca].hu || '0'}%` },
+            { label: 'Angin', value: `${cuaca[idxCuaca].ws || '0'}km/jam` },
+            { label: 'Hujan', value: `${cuaca[idxCuaca].tp || '0'}mm` },
+
+        ].map(d => `
+            <p><span class="label">${d.label}</span> <b class="value">${d.value}</b></p> 
+        `).join("");
+        for (let i = 0; i <= 3; i++) {
+            perkiraanSuhu += `
+    <li>
+        <p class="p-waktu">${formatWaktu(new Date(cuaca[i].local_datetime).getHours())}:${formatWaktu(new Date(cuaca[i].local_datetime).getMinutes())}</p>
+        <p class="p-suhu">${cuaca[i].t}<sup>&deg;</sup></p>
+    </li>`
+        }
+        document.querySelector('.perkiraan').innerHTML = perkiraanSuhu;
+        document.querySelector('.statistik').innerHTML = statistik;
+        document.querySelector('.desa').textContent = `${kecamatan}, ${desa}`
+        document.querySelector('.waktu').textContent = `${hari[date.getDay()]}, ${formatWaktu(date.getHours())}:${formatWaktu(date.getMinutes())}`;
+        document.querySelector('.desc-suhu').innerHTML = `<span>${suhu}<sup>&deg;</sup></span>`;
+        document.querySelector('.kondisi').textContent = weather_desc;
+
+        switch (weather_desc) {
+            case 'Cerah': {
+                document.querySelector('.card-cuaca').style.backgroundImage = `url('./assets/sunny.jpg')`;
                 break;
             }
-            case 'Cerah' : {
-                document.querySelector('.header-card').style.backgroundImage = `url(./assets/sunny.jpg)`;
+            case 'Berawan': {
+                document.querySelector('.card-cuaca').style.backgroundImage = `url('./assets/cloudy.jpg')`;
+                break;
+            }
+            case 'Berawan Tebal': {
+                document.querySelector('.card-cuaca').style.backgroundImage = `url('./assets/rainy.jpg')`;
+                break;
+            }
+            case 'Udara Kabur': {
+                document.querySelector('.card-cuaca').style.backgroundImage = `url('./assets/rainy.jpg')`;
+                break;
+            }
+            case 'Kabut/Asap': {
+                document.querySelector('.card-cuaca').style.backgroundImage = `url('./assets/rainy.jpg')`;
                 break;
             }
         }
-        document.querySelector('.card-cuaca').classList.add('display')
     }
-})
+});
 
